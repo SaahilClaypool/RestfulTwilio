2aaa4c3d2872d08e22b37183dd2d07ec94b89705
saahilclaypool <saahil@claypools.org>
2017-02-18 18:21:25 -0500

    Added texting out

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,34 @@
 import static spark.Spark.*;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+import java.util.HashMap;
 
 public class Main{
+    // Find your Account Sid and Token at twilio.com/user/account
+    public static final String ACCOUNT_SID = "ACc66abeeb6305138885e7991748beed5b";
+    public static final String AUTH_TOKEN = "39dcf1af88bab3d3dbc54a7188d6a3f0";
+
     public static void main(String[] args) {
 	port(8000); 
 	System.out.println("Starting server"); 
-	get("/hello", (req, res) -> "Hello World");
-	    
+	get("/", (req, res) -> "Hello World");
+
+	post("/", (req, res) -> {
+		return "posting"; 
+	    }); 
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Message message = Message
+                .creator(new PhoneNumber(" +19787601330"), new PhoneNumber("+19788505030"),
+                        "This is from TWILIO! what up isaac")
+                .create();
+
+        System.out.println(message.getSid());
+
+	System.out.println("end of main"); 
     }
     
+
+
 }
