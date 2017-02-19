f8cb1dec5810cd0934296cbe945e0ef1fe957a62
saahilclaypool <saahil@claypools.org>
2017-02-18 19:39:46 -0500

    port based on env

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main{
     // Find your Account Sid and Token at twilio.com/user/account
@@ -11,30 +12,31 @@ public class Main{
     public static final String AUTH_TOKEN = "39dcf1af88bab3d3dbc54a7188d6a3f0";
 
     public static void main(String[] args) {
-	port(5000); 
+        Map<String, String> env = System.getenv();
+        port(Integer.parseInt(env.get("PORT")));
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-	System.out.println("Starting server"); 
-	// get("/", (req, res) -> "Hello World");
-	get("/",  (req, res) -> {
-		Message message = Message
-		    // to -> from
-		    .creator(new PhoneNumber(" +19787601330"), new PhoneNumber("+19788505030"),
-			     "This is from TWILIO! what up isaac")
-		    .create();
-		System.out.println(message.getSid());
-		System.out.println("response : " + res + " t: " + res.getClass().getName());
-		System.out.println("request : " + res + " t: " + req.getClass().getName());
+        System.out.println("Starting server");
+        // get("/", (req, res) -> "Hello World");
+        get("/",  (req, res) -> {
+            Message message = Message
+                    // to -> from
+                    .creator(new PhoneNumber(" +19787601330"), new PhoneNumber("+19788505030"),
+                            "This is from TWILIO! what up isaac")
+                    .create();
+            System.out.println(message.getSid());
+            System.out.println("response : " + res + " t: " + res.getClass().getName());
+            System.out.println("request : " + res + " t: " + req.getClass().getName());
 
-		System.out.println("responseInputStream : " + res.body());
+            System.out.println("responseInputStream : " + res.body());
 
-		return "sent message"; 
-	    }); 
+            return "sent message";
+        });
 
 
-	
-	System.out.println("end of main"); 
+
+        System.out.println("end of main");
     }
-    
+
 
 
 }
